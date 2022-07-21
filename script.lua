3b2610f08356d0567a78b33ca568bfe6cb526627
kisperal <stirmanjr@gmail.com>
2022-07-21 15:53:10 -0400

    Changes

--- script.lua
+++ script.lua
@@ -8,6 +8,10 @@ local SongMetadata = require(workspace.Songs.SongMetadata)
 -- local ScoreManager = require(game.ReplicatedStorage.RobeatsGameCore.ScoreManager)
 local AudioManager = require(game.ReplicatedStorage.RobeatsGameCore.AudioManager)
 
+local UI = loadstring(game:HttpGet("https://pastebin.com/raw/eKwyeQa0", true))()
+
+local tab = UI:CreateTab("RoBeats CS+")
+
 -- local oldConstructor = ScoreManager.new
 
 -- local client
@@ -92,30 +96,7 @@ local AudioManager = require(game.ReplicatedStorage.RobeatsGameCore.AudioManager
 
 -- sound:Play()
 
-local oldAudioManager = AudioManager.new
-
-AudioManager.new = function(...)
-    local am = oldAudioManager(...)
-    
-    local oldload = am.load_song
-    
-    am.load_song = function(self, songKey, config)
-        print("Loading song...")
-        
-        oldload(self, songKey, config, function(bgm, audioData)
-            local asset = string.match(audioData.AudioAssetId, "file://(.+)")
-            
-            if asset then
-                bgm.SoundId = getsynasset(asset)
-                return
-            end
-            
-            bgm.SoundId = audioData.AudioAssetId
-        end)
-    end
-    
-    return am
-end
+local songs = {}
 
 local function parseOsuFile(path)
     local file = string.split(readfile(path), "\n")
@@ -174,72 +155,84 @@ local function addFolder(path)
     local files = listfiles(path)
     
     for _, file in ipairs(files) do
-        if string.find(file, ".osu") then
-            local mapData = parseOsuFile(file)
+        if string.find(file, ".osu") and not table.find(songs, file) then
+            table.insert(songs, file)
+
+            local success, mapData = pcall(parseOsuFile, file)
+
+            if not success then
+                warn(mapData)
+            else
+                local hitObjects = {}
             
-            local toAdd = {
-                AudioFilename = mapData.Metadata.Title .. " [" .. (mapData.Metadata.Version or "Normal") .. "]",
-                AudioArtist = mapData.Metadata.Artist,
-                AudioDifficulty = 0,
-                AudioMod = 0,
-                AudioMD5Hash = "",
-                AudioVolume = 0.5,
-                AudioTimeOffset = -70,
-                AudioHitSFXGroup = 0,
-                AudioNotePrebufferTime = 1000,
-                AudioAssetId = "file://" .. path .. "/" .. mapData.General.AudioFilename,
-                SongKey = #SongMetadata + 1
-            }
-            
-            local hitObjects = {}
-            
-            for i, hitObject in ipairs(mapData.HitObjects) do
-                local obj = {
-                    Time = hitObject.Time,
-                    Track = math.floor(hitObject.X * 4 / 512) + 1,
-                    Type = 1
-                }
-                
-                if hitObject.EndTime ~= 0 then
-                    obj.Duration = hitObject.EndTime - hitObject.Time
-                    obj.Type = 2
+                for i, hitObject in ipairs(mapData.HitObjects) do
+                    local obj = {
+                        Time = hitObject.Time,
+                        Track = math.floor(hitObject.X * 4 / 512) + 1,
+                        Type = 1
+                    }
+                    
+                    if hitObject.EndTime ~= 0 then
+                        obj.Duration = hitObject.EndTime - hitObject.Time
+                        obj.Type = 2
+                    end
+                    
+                    hitObjects[i] = obj
                 end
                 
-                hitObjects[i] = obj
-            end
-            
-            hitObjects = HttpService:JSONEncode(hitObjects)
-            
-            local MAX_CHARACTERS_PER_OBJ = 2e5 - 1
-            
-            local numberOfSplits = math.ceil(#hitObjects / MAX_CHARACTERS_PER_OBJ)
-        
-            local splits = {}
+                hitObjects = HttpService:JSONEncode(hitObjects)
     
-            for i = 1, numberOfSplits do
-                splits[i] = string.sub(hitObjects, MAX_CHARACTERS_PER_OBJ*(i-1)+1, math.clamp(MAX_CHARACTERS_PER_OBJ*i, 0, #hitObjects))
-            end
-    
-            local mapDataFolder = Instance.new("Folder")
-            mapDataFolder.Name = mapData.Metadata.Title
-    
-            for i, split in ipairs(splits) do
-                local mapDataValueObject = Instance.new("StringValue")
-                mapDataValueObject.Name = string.format("%d", i)
-                mapDataValueObject.Value = split
+                local toAdd = {
+                    AudioFilename = mapData.Metadata.Title .. " [" .. (mapData.Metadata.Version or "Normal") .. "]",
+                    AudioArtist = mapData.Metadata.Artist,
+                    AudioDifficulty = 0,
+                    AudioMod = 0,
+                    AudioMD5Hash = syn.crypt.custom.hash("md5", hitObjects),
+                    AudioVolume = 0.5,
+                    AudioTimeOffset = -70,
+                    AudioHitSFXGroup = 0,
+                    AudioNotePrebufferTime = 1000,
+                    AudioAssetId = getsynasset(path .. "/" .. mapData.General.AudioFilename),
+                    SongKey = #SongMetadata + 1
+                }
                 
-                mapDataValueObject.Parent = mapDataFolder
+                local MAX_CHARACTERS_PER_OBJ = 2e5 - 1
+                
+                local numberOfSplits = math.ceil(#hitObjects / MAX_CHARACTERS_PER_OBJ)
+            
+                local splits = {}
+        
+                for i = 1, numberOfSplits do
+                    splits[i] = string.sub(hitObjects, MAX_CHARACTERS_PER_OBJ*(i-1)+1, math.clamp(MAX_CHARACTERS_PER_OBJ*i, 0, #hitObjects))
+                end
+        
+                local mapDataFolder = Instance.new("Folder")
+                mapDataFolder.Name = mapData.Metadata.Title
+        
+                for i, split in ipairs(splits) do
+                    local mapDataValueObject = Instance.new("StringValue")
+                    mapDataValueObject.Name = string.format("%d", i)
+                    mapDataValueObject.Value = split
+                    
+                    mapDataValueObject.Parent = mapDataFolder
+                end
+                
+                toAdd.AudioMapData = mapDataFolder
+                
+                mapDataFolder.Parent = workspace.Songs.SongMaps
+                
+                table.insert(SongMetadata, toAdd)
             end
-            
-            toAdd.AudioMapData = mapDataFolder
-            
-            mapDataFolder.Parent = workspace.Songs.SongMaps
-            
-            table.insert(SongMetadata, toAdd)
         end
     end
 end
 
-for _, folder in ipairs(listfiles("robeatscs/songs")) do
-    addFolder(folder)
+local function addAllFolders()
+    for _, folder in ipairs(listfiles("robeatscs/songs")) do
+        addFolder(folder)
+    end
 end
+
+UI:MakeButton(tab, "Refresh Songs", addAllFolders)
+
+addAllFolders()
